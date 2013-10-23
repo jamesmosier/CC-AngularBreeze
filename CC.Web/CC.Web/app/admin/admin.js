ce5995879dc566163a354cc549a611eeb143c1c0
James Mosier <jamesdmosier@gmail.com>
2013-10-22 20:38:24 -0400

    added EF and HotTowel. Erros with breeze.WebAPI

---
+++ CC.Web/CC.Web/app/admin/admin.js
@@ -0,0 +1,21 @@
+﻿(function () {
+    'use strict';
+    //controllerID is called on the view (admin.html) and has to be the same name
+    var controllerId = 'admin';
+    angular.module('app').controller(controllerId, ['common', admin]);
+
+    function admin(common) {
+        var getLogFn = common.logger.getLogFn;
+        var log = getLogFn(controllerId);
+
+        var vm = this;
+        vm.title = 'Admin';
+
+        activate();
+
+        function activate() {
+            common.activateController([], controllerId)
+                .then(function () { log('Activated Admin View'); });
+        }
+    }
+})();
